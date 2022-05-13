aa25b94515f83a76033aa81ff0029cf24e3be98e
User <User@DESKTOP-VRB87UF>
2022-05-13 13:20:41 +0300

    Добавьте файлы проекта.

---
+++ file1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Text.Unicode;
+
+    class Program 
+{ 
+    static void Main(string[] args)
+    { 
+        string path = @"C:\Users\User\source\repos\file1\file1\text.txt";
+        string vpath = @"..\..\..\..\Text.txt";
+        FileInfo info = new FileInfo(path);
+        int count = System.IO.File.ReadAllLines(path).Length;
+        string s = ""; string[] textMass; 
+        StreamReader sr = new StreamReader(path);
+        while (sr.EndOfStream != true) 
+        { 
+            s += sr.ReadLine();
+        } 
+        textMass = s.Split(' '); 
+        sr.Close(); 
+        var es = s.Any(ch => ch >= UnicodeRanges.Cyrillic.FirstCodePoint && ch < (UnicodeRanges.Cyrillic.FirstCodePoint + UnicodeRanges.Cyrillic.Length));
+        var res = s.Any(ch => ch >= UnicodeRanges.BasicLatin.FirstCodePoint && ch < (UnicodeRanges.BasicLatin.FirstCodePoint + UnicodeRanges.BasicLatin.Length)); 
+        var result = "result.txt"; 
+        using (StreamWriter streamWriter = new StreamWriter(result)) 
+        { 
+            streamWriter.WriteLine("Название файла " + info.Name);
+            streamWriter.WriteLine("Абсолютный путь: " + info.FullName);
+            streamWriter.WriteLine("Относительный путь: " + vpath);
+            streamWriter.WriteLine("Время создания " + info.CreationTime);
+            streamWriter.WriteLine("Размер файла " + info.Length);
+            streamWriter.WriteLine("Присутствуют ли в тексте цифры: " + s.Count(char.IsNumber)); 
+            if (s.Count(char.IsNumber) > 0)
+            { 
+                streamWriter.WriteLine("Есть цифры"); 
+            }
+            else
+            { 
+                streamWriter.WriteLine("Нет цифр"); 
+            }
+            streamWriter.WriteLine("Количество слов: " + textMass.Length);
+            streamWriter.WriteLine("Количество строк: " + count);
+            streamWriter.WriteLine(es ? "Есть кириллица" : "Нет кириллицы"); 
+            streamWriter.WriteLine(res ? "Есть латиница" : "Нет латиницы"); 
+        } 
+    }
+} 
+
+
---
+++ file2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using System.Text.Encodings;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+    var path = "Table.xlsx";
+    //Регистрация провайдера кодировок
+    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+    //регистрация кодировки  для поддержки кириллицы
+    Encoding encoding = Encoding.GetEncoding(1251);
+
+var lines = File.ReadAllLines(path);
+Console.WriteLine(lines);
+var workers = new Worker[lines.Length - 1];
+
+for (int i = 1; i < lines.Length; i++)
+{
+    var splits = lines[i].Split(';');
+    var worker = new Worker();
+    worker.Фамилия = splits[0];
+    worker.Возраст = Convert.ToDouble(splits[1]);
+    worker.Опыт_работы = Convert.ToDouble(splits[2]);
+    worker.Заработная_плата = Convert.ToDouble(splits[3]);
+    workers[i - 1] = worker;
+    //Console.WriteLine(worker);
+}
+var result = "result.csv";
+using (StreamWriter streamWriter = new StreamWriter(result, false, encoding))
+{
+    streamWriter.WriteLine($"Name;Height;Weight;Coef");
+    for (int i = 0; i < workers.Length; i++)
+    {
+        streamWriter.WriteLine(workers[i].ToExcel());
+    }
+}
+
+var jsonOptions = new JsonSerializerOptions()
+{
+    Encoder = JavaScriptEncoder.Default
+};
+
+var json = JsonSerializer.Serialize(workers, jsonOptions);
+File.WriteAllText("result.json", json);
+
+var stringJson = File.ReadAllText("result.json");
+var array = JsonSerializer.Deserialize<Worker[]>(stringJson);
+foreach (var item in array)
+{
+    Console.WriteLine(item.ToString());
+}
+
+string jsonNewtonsoft = Newtonsoft.Json.JsonConvert.SerializeObject(workers, Newtonsoft.Json.Formatting.Indented);
+
+File.WriteAllText("NewtonsoftResult.json", jsonNewtonsoft);
+
+public class Worker
+{
+    public string Фамилия { get; set; }
+    public double Возраст { get; set; }
+    public double Опыт_работы { get; set; }
+
+    public double Заработная_плата { get; set; }
+
+    public double Соотношение_опыта_работы_и_заработной_платы { get => Заработная_плата * Опыт_работы; }
+    public double Премия_сотрудника { get => (Опыт_работы * Заработная_плата)/100; }
+    public override string ToString()
+    {
+        return $" {Фамилия}\n Возраст : {Возраст}\n Опыт работы: {Опыт_работы} \n Заработная плата: {Заработная_плата} \n Соотношение опыта работы и заработной платы:{Соотношение_опыта_работы_и_заработной_платы} \n Премия сотрудника:{Премия_сотрудника} рублей\n";
+    }
+
+    public string ToExcel()
+    {
+        return $" {Фамилия};{Возраст};{Опыт_работы};{Заработная_плата};{Соотношение_опыта_работы_и_заработной_платы};{Премия_сотрудника}";
+
+    }
+}
